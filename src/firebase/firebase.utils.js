7b35cbb80b34e7cc6edd099d29b92dd14fd91a6d
Varsha Ravindra <varsharavindra12@gmail.com>
2021-01-08 16:29:17 +0530

    Storing user data in Firebase and in our app

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,6 +11,31 @@ const config = {
     appId: "1:443144461095:web:8fbedf0ad2bc615641e702"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapshot = await userRef.get();
+
+    if(!snapshot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch(error) {
+            console.log('Error creating user', error.message);
+        }
+    }
+
+    return userRef;
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
